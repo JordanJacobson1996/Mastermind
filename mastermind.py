ae0be29c431ec06e613ca8f4b8aaea642a4c5d5b
Jordan Jacobson <jordandurjacobson@gmail.com>
2024-12-07 06:52:04 +1100

    Added loop to check guess against code and add white, black or null pegs for every letter in the guess to the board

--- mastermind.py
+++ mastermind.py
@@ -3,6 +3,7 @@ import random
 #Generates random code based on the six letters available.
 code = "".join(random.sample('ABCDEF', 4))
 numGuesses = 0
+board = "" #For displaying the correct pegs for the guesses, black for correct letter and position and white for letter in code.
 
 guess = input("Please enter your first guess, must be four letter from ABCDEF and have no repeated letters:  ").upper()
 
@@ -10,4 +11,18 @@ while len(guess) > 4:
     print("You have entered too many letters, please try again")
     guess = input("Please enter a guess: ")
 
+#Loop to check positions of guess letters against code letters and add results to board.
+i = 0
+while i < len(guess):
+    if guess[i] == code[i]:
+        board = board + "B"
+    elif guess [i] in code:
+        board = board + "W"
+    else:
+        board = board + "X"
+    i += 1
+
+print(code)
 print(guess)
+print(board)
+
