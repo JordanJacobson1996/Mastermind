ed20af843570f85c612187bcc7f1ad6ea19805b9
Jordan Jacobson <jordandurjacobson@gmail.com>
2024-12-07 07:32:57 +1100

    Combined all attributes into one while loop that tests if count is below twelve

--- mastermind.py
+++ mastermind.py
@@ -11,18 +11,30 @@ while len(guess) > 4:
     print("You have entered too many letters, please try again")
     guess = input("Please enter a guess: ")
 
+#Main loop that iterates through 12 guesses or ends if the player gets the code correct.
+while numGuesses < 12:
 #Loop to check positions of guess letters against code letters and add results to board.
-i = 0
-while i < len(guess):
-    if guess[i] == code[i]:
-        board = board + "B"
-    elif guess [i] in code:
-        board = board + "W"
-    else:
-        board = board + "X"
-    i += 1
+    i = 0
+    board = ""
+    while i < len(guess):
+        if guess[i] == code[i]:
+            board = board + "B"
+        elif guess [i] in code:
+            board = board + "W"
+        else:
+            board = board + "X"
+        i += 1
+    numGuesses += 1
+    print(guess)
+    print(board)
 
-print(code)
-print(guess)
-print(board)
+    #Checks if player has won or lost.
+    if board == "BBBB":
+        result = 'The code was ', code, '. The player guessed it in ', numGuesses, ' guesses.'
+        break
+    elif numGuesses == 12:
+        result = "Player failed to guess the code. The code was ", code
+        break
+    guess = input("Guess incorrect, make another guess: ")
 
+print(result)
