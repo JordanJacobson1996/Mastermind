cb17a609af2095df1a54b377ef10dd912e4ce0d5
Jordan Jacobson <jordandurjacobson@gmail.com>
2024-12-07 06:34:13 +1100

    Check length of guess to make sure it is only 4 letters

--- mastermind.py
+++ mastermind.py
@@ -2,5 +2,12 @@ import random
 
 #Generates random code based on the six letters available.
 code = "".join(random.sample('ABCDEF', 4))
+numGuesses = 0
 
-print(code)
+guess = input("Please enter your first guess, must be four letter from ABCDEF and have no repeated letters:  ").upper()
+
+while len(guess) > 4:
+    print("You have entered too many letters, please try again")
+    guess = input("Please enter a guess: ")
+
+print(guess)
